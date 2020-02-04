a4e04c2a73a92bf20c60d4ce22a0dfa0afc1591f
Justin <j6309j6@hotmail.com>
2020-02-04 15:25:25 +0800

    project

---
+++ viewmodel/viewmodel/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace viewmodel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Logon()
+        {
+            return View();
+        }
+               
+
+        public ActionResult firstpage()
+        {
+            
+
+            return View();
+        }
+    }
+}
